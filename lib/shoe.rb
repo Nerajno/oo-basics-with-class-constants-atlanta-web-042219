669bcf19c053ded611ae3c5ccbd5ee691b7adc0c
github username <github email address>
2019-05-24 09:21:32 -0400

    round_2 - still a bit lost

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,23 +3,24 @@ require 'pry'
 
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  # attr_reader 
 
   BRANDS = []
 
   def initialize(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand) #when did we learn this ?
   end
+
+  # def brand=(brand)
+  #   @brand = brand
+  #  BRANDS << brand.uniq
+  # end
+  
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-  # def brand=(brand)
-  #   @brand = brand
-    
-  # end
 
 end
